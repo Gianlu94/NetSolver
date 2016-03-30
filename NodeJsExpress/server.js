89cfac1fb31408319e59e9680e73ff39c8536868
gianLuke <sanar@live.it>
2016-03-30 10:58:56 +0200

    Some changes on server js

--- NodeJsExpress/server.js
+++ NodeJsExpress/server.js
@@ -15,8 +15,11 @@ app.use(express.static('/NetSolver/web/css'));
 app.use(express.static('/NetSolver/web/js'));
 */
 
+//define static files
+app.use(express.static(__dirname+"/NetSolver/web/css" ));
 app.use(express.static(__dirname+"/NetSolver/web/js" ));
 
+//manage  the request of a network problem
 app.get("/Tracer", function(req,res){
 	 fs.readFile(path.join(__dirname+"/Traces/trac1.txt"), function(err, file) {  
             if(err) {  
@@ -24,30 +27,44 @@ app.get("/Tracer", function(req,res){
                 return;  
             }
             var array = file.toString().split(" ");
-            //var array2;
+            var array2 ="";
+            
    		    for(var i = 0; i < array.length;i++) {
-   		    	if (array[i]="-network-"){
+   		    	switch (array[i]){
+   		    		case "-network-" :
+   		    			array[i]="192.168.100.0/28";
+   		    			break;
+   		    		case "-hosts-" :
+   		    			array[i]="5 hosts";
+   		    			break;
+   		    		default : break;
+   		    	}
+   		    	/*if (array[i]=="-network-"){
+   		    		//console.log(i+"  "+ array[i]+"\n");
    		    		array[i]="192.168.100.0/28";
    		    	}
    		    	else{
    		    		//array2[i]=array[i]+" ";
    		    		console.log(i+"  "+ array[i]+"\n");
    		    	}
-   		    	/*else{
+   		    	  else{
    		    		//array2[i]=array[i]+" ";
    		    		//console.log(i+"  "+ array[i]+"\n");
    		    	}*/
-    		}  
+   		    	array2 = array2 +" "+array[i];
+    		} 
             res.writeHead(200, { 'Content-Type': 'text/html' });  
-            res.end(array.toFile, "utf-8");  
+            res.end(array2.toString(), "utf-8");  
         });
 	//res.sendFile(path.join(__dirname+"/Tracer/trac1.txt"));
 });
 
+//load homePage
 app.get("/NetSolver/web/homeNetSolver.html", function(req,res){
 	res.sendFile(path.join(__dirname+"/NetSolver/web/homePageNetSolver.html"));
 });
 
+//server up
 app.listen(3000, function(){
 	  console.log('Server up: http://localhost:3000');
 });
