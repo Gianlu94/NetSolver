4bde32a216fd0e3420dbf9582f36494ad5d2b2ad
gianLuke <sanar@live.it>
2016-04-02 11:22:35 +0200

    Simple xml Creation

--- NodeJsExpress/server.js
+++ NodeJsExpress/server.js
@@ -6,6 +6,7 @@ var querystring = require("query-string");
 var url = require("url");
 var path = require("path");
 var fs = require("fs");
+var XmlWriter = require('xml-writer');
 
 /*app.use(bodyParser.urlencoded({
   extended: false
@@ -28,32 +29,80 @@ app.use(express.static(__dirname+"/NetSolver/web/css" ));
 app.use(express.static(__dirname+"/NetSolver/web/js" ));
 
 
+function createXmlProblem(arrayXml){
+	/*var XmlProblem;
+	 XmlProblem="<NetworkProblem>\n"
+	 XMlProblem="
+	 var eleMain = xml.ele('Networks');
+	 for(var i = 0; i < arrayXml.length;i++) {
+   		switch (array[i]){
+   			case "-network" :
+   		    	eleMain.ele('Network',{'address':array[i+1]});
+   		    	break;
+   		    case "-hosts-" :
+   		    	eleMain.ele('Hosts',array[i+1]);
+   		    	break;
+   		    default : break;
+   		}    	
+    }*/
+    var xw = new XmlWriter(true);
+	xw.startDocument();
+	xw.startElement('NetworkProblem');
+	for(var i = 0; i < arrayXml.length;i++) {
+		console.log(arrayXml[i]+"\n");
+   		switch (arrayXml[i]){
+   			case "-network" :
+   				xw.startElement('Network');
+   				xw.text(arrayXml[i+1]);
+   		    	xw.endElement();
+   		    	break;
+   		    case "-hosts" :
+   		    	xw.startElement('Hosts');
+   				xw.text(arrayXml[i+1]);
+   		    	xw.endElement();
+   		    	break;
+   		    default : break;
+   		}    	
+    }
+	xw.endElement();
+	xw.endDocument();
+	fs.writeFile(__dirname+'/Traces/Trace1/trac1.xml', xw.toString(), function (err) {
+  	if (err) return console.log(err);
+  		console.log("Creation oks");
+	});
+	//console.log(xw.toString());
+}
 function DifficultyFile (difficulty,req,res) {
 	console.log("PARAM "+ difficulty);
 	switch (difficulty){
 		case '1' :
-			fs.readFile(path.join(__dirname+"/Traces/trac1.txt"), function(err, file) {  
+			fs.readFile(path.join(__dirname+"/Traces/Trace1/trac1.txt"), function(err, file) {  
             if(err) {  
                 // write an error response or nothing here  
                 return;  
             }
             var array = file.toString().split(" ");
-            var array2 ="";
-            
+            var arrayClient ="";
+            var arrayXml = "";
    		    for(var i = 0; i < array.length;i++) {
    		    	switch (array[i]){
    		    		case "-network-" :
    		    			array[i]="192.168.100.0/28";
+   		    			arrayXml = arrayXml +"-network "+array[i];
    		    			break;
    		    		case "-hosts-" :
    		    			array[i]="5 hosts";
+   		    			arrayXml = arrayXml +" -hosts "+array[i];
    		    			break;
-   		    		default : break;
+   		    		default : 
+   		    			break;
    		    	}
-   		    	array2 = array2 +" "+array[i];
-    		} 
+   		    	arrayClient = arrayClient +" "+array[i];
+    		}
+    		console.log(arrayXml);
+    		createXmlProblem(arrayXml.split(" "));
             res.writeHead(200, { 'Content-Type': 'text/html' });  
-            res.end(array2.toString(), "utf-8");  
+            res.end(arrayClient.toString(), "utf-8");  
         });
 		break;
 		default : break;
