92e4ff15982d464d4ee8aa6010027938b84966cb
gianLuke <sanar@live.it>
2016-06-25 19:55:18 +0200

    Create Problem js created

---
+++ create_problems.js
@@ -0,0 +1,209 @@
+/*
+ This file manages the creation of the network problems (XML and txt)
+ */
+
+var querystring = require("query-string");
+var path = require("path");
+var url = require("url");
+var fs = require("fs");
+var XmlWriter = require('xml-writer');
+var networkProblem = require("./problem.js");
+
+
+var networkGiven = [];
+
+module.exports = {
+
+	problemPath : "",
+
+	createXmlProblem : function(arrayXml, difficultyP){
+		var xw = new XmlWriter(true);
+		xw.startDocument();
+		xw.startElement('Problem');
+		for(var i = 0; i < arrayXml.length;i++) {
+			
+			switch (arrayXml[i]){
+				case "-networkProblems" :
+					xw.startElement('NetworkProblem');
+					arrayXml[i]=" ";
+					break;
+				case "-number" :
+					xw.startElement('Number');
+					xw.text(arrayXml[i+1]);
+					xw.endElement();
+					break;
+				case "-network" :
+					xw.startElement('Network');
+					xw.text(arrayXml[i+1]);
+					xw.endElement();
+					break;
+				case "-hosts" :
+					xw.startElement('Hosts');
+					xw.text(arrayXml[i+1]);
+					xw.endElement();
+					break;
+				case "-networkProbleme" :
+					xw.endElement();
+					arrayXml[i]=" ";
+					break;
+				case "-switch" :
+					xw.startElement('Switch');
+					xw.text(arrayXml[i+1]);
+					xw.endElement();
+					break;
+				case "-vlans" :
+					xw.startElement('Vlan');
+					xw.text(arrayXml[i+1]);
+					xw.endElement();
+					break;
+				case "-hubs" :
+					xw.startElement('Hub');
+					xw.text(arrayXml[i+1]);
+					xw.endElement();
+					break;
+				default : break;
+			}
+		}
+		xw.endElement();
+		xw.endDocument();
+		fs.writeFile(__dirname+difficultyP, xw.toString(), function (err) {
+			if (err) return console.log(err);
+			console.log("Creation oks");
+		});
+		
+	},
+
+	//support function used to give different networks
+	networkAlreadyGiven : function(network){
+		var trovato = false;
+	
+		for (var i = 0; i < networkGiven.length && !trovato; i++){
+			var networkInArray = networkGiven[i];
+			if (network.toString().localeCompare(networkInArray) == 0){
+				trovato = true;
+			}
+		}
+		if (trovato){
+			return true;
+		}
+		else{
+			networkGiven.push(network);
+			return false;
+		}
+	},
+
+	//get network address from NetworkAddress.txt
+	setNetworkAddress : function (arrayN){
+		var foundNetwork = false;
+		var randomNetworkToSend;
+		while (!foundNetwork) {
+			var randomNetworkToSend = parseInt(Math.random() * 3);
+			console.log("I would like to send network in position " + randomNetworkToSend);
+			
+			if (!module.exports.networkAlreadyGiven(networkProblem.normalizeNetworkAddress(arrayN[randomNetworkToSend]))){
+				foundNetwork = true;
+			}
+			
+		}
+		return arrayN[randomNetworkToSend];
+	},
+
+	/*This function selects the trace of problem to send to the student (related to
+	  the difficulty he gave) and creates support array.
+	 */
+	difficultyFile : function (difficulty, req, res){
+
+		networkGiven.length = 0;
+		console.log("PARAM "+ difficulty);
+		var randomFileToSend;
+		switch (difficulty){
+			case '1' :
+				randomFileToSend = parseInt(((Math.random() * 3) + 1));
+				console.log("RandomFileTo send" + randomFileToSend);
+				break;
+			case '2' :
+				randomFileToSend = parseInt(((Math.random() * 1) + 1));
+				console.log("RandomFileTo send" + randomFileToSend);
+				break;
+			default : break;
+		}
+		fs.readFile(path.join(__dirname+"/Traces/Trace"+difficulty+"/trac"+randomFileToSend+".txt"), function(err1, file1) {
+			module.exports.problemPath="/Traces/Trace"+difficulty+"/trac"+randomFileToSend+".xml";
+			if(err1) {
+				// write an error response or nothing here
+				return;
+			}
+			else {
+				fs.readFile(path.join(__dirname+"/ServerRes/NetworkAddress.txt"), function(err2, file2) {
+					if (err2){
+						// write an error response or nothing here
+						return;
+					}
+					else {
+						var array = file1.toString().split(" ");
+						var arrayClient = "";
+						var arrayXml = "";
+						var array2 = file2.toString().split('\n');
+
+						var numberVlan;
+
+						for (var i = 0; i < array.length; i++) {
+							array[i] = array[i].trim();
+							switch (array[i]) {
+								case "-network-" :
+									array[i] = module.exports.setNetworkAddress(array2);
+									arrayXml = arrayXml + "-network " + array[i];
+									break;
+								case "-number-" :
+									array[i] = "";
+									arrayXml = arrayXml + " -number " + array[i + 1];
+									//First Simple Solution
+									numberVlan = array[i+1];
+									array[i + 1] = "";
+									break;
+								case "-hosts-" :
+									var numberHosts = Math.floor((Math.random() * 1) + 2);
+									array[i] = numberHosts + " hosts";
+									arrayXml = arrayXml + " -hosts " + array[i];
+									break;
+								case "-switch-" :
+									var numberSwitch = Math.floor((Math.random() * 3) + 1);
+									array[i] = numberSwitch + " switch";
+									arrayXml = arrayXml + " -switch " + array[i];
+									break;
+								case "-vlans-" :
+									array[i] = numberVlan + " vlan";
+									arrayXml = arrayXml + " -vlans " + array[i];
+									break;
+								case "-hub-" :
+									//to demostrate work
+									var numberHub = 1
+									array[i] = numberHub + " hub";
+									arrayXml = arrayXml + " -hubs " + array[i];
+									break;
+								case "-networkProblems-" :
+									array[i] = "";
+									arrayXml = arrayXml + " -networkProblems ";
+									break;
+								case "-networkProbleme-" :
+									array[i] = "";
+									arrayXml = arrayXml + " -networkProbleme ";
+									break;
+								case "-wrap-" :
+									array[i] = "\n";
+									break;
+								default :
+									break;
+							}
+							arrayClient = arrayClient + " " + array[i];
+						}
+						module.exports.createXmlProblem(arrayXml.split(" "), module.exports.problemPath);
+						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.end(arrayClient.toString(), "utf-8");
+					}
+				});
+			}
+		});
+	}
+	
+}
